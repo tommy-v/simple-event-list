5e3039af778112e7e675eec8f7c0aa5f09c56ccf
Thomas Vandercam <tvandercam@gmail.com>
2020-05-23 11:42:10 +0200

    init(*): project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'calendar', pathMatch: 'full'
+  },
+  {
+    path: 'calendar', loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
