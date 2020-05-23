34b45c86b4b2208c3a0fa5bdf6f26d93c374faea
Thomas Vandercam <tvandercam@gmail.com>
2020-05-23 15:44:32 +0200

    feat(calendar): create event-card component

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
-import { CalendarComponent } from '../calendar/calendar.component';
+import { CalendarComponent } from './calendar.component';
+import { EventCardComponent } from './event-card/event-card.component';
 
 
 @NgModule({
-  declarations: [CalendarComponent],
+  declarations: [CalendarComponent, EventCardComponent],
   imports: [
     CommonModule,
     CalendarRoutingModule
