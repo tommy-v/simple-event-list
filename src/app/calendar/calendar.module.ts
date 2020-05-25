aedc2a3e6f72a65dc37609fb04ea257d24dbe69e
Thomas Vandercam <tvandercam@gmail.com>
2020-05-25 17:40:36 +0200

    feat(events): add loading

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -6,13 +6,15 @@ import { CalendarComponent } from './calendar.component';
 import { EventCardComponent } from './event-card/event-card.component';
 
 import { NgxMasonryModule } from 'ngx-masonry';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CalendarComponent, EventCardComponent],
   imports: [
     CommonModule,
     CalendarRoutingModule,
-    NgxMasonryModule
+    NgxMasonryModule,
+    SharedModule
   ]
 })
 export class CalendarModule { }
