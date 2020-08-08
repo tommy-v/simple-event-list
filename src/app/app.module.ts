9a6271aa29831033d0ece85c2848c7c014b3f719
Thomas Vandercam <tvandercam@gmail.com>
2020-08-08 16:28:43 +0200

    feat(app): enable interceptor and mock api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+
+// Utils
+import { HttpRequestInterceptorMock } from './core/services/http-request-interceptor.mock';
 
 @NgModule({
   declarations: [
@@ -17,7 +23,13 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptorMock,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
